ad248dea17d14467a5c5837b62bb8bd83c0a2394
Abel Quintero <abel.j.quintero@gmail.com>
2018-03-21 23:41:27 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-  word.match(/\b[aeiouAEIOU]/)
+  if word.match(/\b[aeiouAEIOU]/)
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
